f2d259b3433abe114163c8b38ae94efd866e643e
PC <PC@DESKTOP-CSACRG6>
2023-12-12 13:47:13 +0900

    초기 커밋.

---
+++ App.xaml.h
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "App.xaml.g.h"
+#include "BgLabelControl.h"
+
+namespace winrt::BgLabelControlApp::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+
+    private:
+        winrt::Microsoft::UI::Xaml::Window window{ nullptr };
+    };
+}
